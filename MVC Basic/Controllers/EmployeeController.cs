d4adf5ed88ad0c89a2b397633d445afa154b1951
Aanchal Mehta <aanchal.mehta@nagarro.com>
2021-08-18 08:55:39 +0530

    Add project files.

---
+++ MVC Basic/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Basic.Models;
+
+namespace MVC_Basic.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        /*public string Profile(int id ) 
+        {
+            string profile =string.Empty;
+            if (id == 1)
+            {
+                profile = "Employee 1 profile";
+
+            }
+            else if (id == 1)
+            {
+                profile = "Employee 2 profile";
+
+            }
+            else {
+                profile = "No record found";
+            }
+            return  profile; 
+        }*/
+        public ActionResult Index() 
+        {
+            var data = GetEmployee();
+            return View(data);
+        }
+        private Employee GetEmployee() 
+        {
+            return new Employee()
+            {
+                Id = 1,
+                Name = "Anchal ",
+                Address = "Palasia Indore"
+            };
+        }
+
+
+   
+    }
+}
---
+++ MVC Basic/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Basic.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index1()
+        {
+            return "Hello inside controller";
+        }
+        public string Name() 
+        { 
+            return "Hello Anchal"; 
+        }
+        public ActionResult Index() { return View(); }
+    }
+}
